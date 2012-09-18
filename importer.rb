edffdeedb81e66a5508c40775696c20909e25ebd
lcarsos <lcarsos@gmail.com>
2012-09-17 20:46:59 -0600

    changing transcript to use new format, replacing quotes

--- importer.rb
+++ importer.rb
@@ -6,15 +6,21 @@ require 'uri'
 require 'json'
 
 def parse_transcript(text)
-  text.gsub! /(?<!\n)\n(?!\n)/, "\n\n"
+  #text.gsub! /(?<!\n)\n(?!\n)/, "\n"
   text.gsub! /\[\[/, "["
   text.gsub! /\]\]/, "]"
-  length = text.lines("\n").to_a.length - 3
-  text = text.lines("\n").to_a[0..length].join
+  text.gsub! /\&\#39;/, "'"
+  text.gsub! /\&quot;/, '"'
+  lines = text.lines("\n").to_a
+  length = lines.length - 3
+  lines.each {|l| l.insert 0, ":" }
+  text = lines[0..length].join
   text.chomp!
 end
 
 def parse_titletext(text)
+  text.gsub! /\&\#39;/, "'"
+  text.gsub! /\&quot;/, '"'
   text.gsub /''/, "'<nowiki />'"
 end
 
@@ -33,7 +39,7 @@ ARGV.each do |arg|
   #puts xkcdjson["transcript"]
 
   explain_url = "http://explainxkcd.com/#{xkcdjson["year"]}/#{"%02d" % xkcdjson["month"]}/#{"%02d" % xkcdjson["day"]}/#{safe_name}/"
-  #puts explain_url
+  puts explain_url
   #explainURI = URI.parse explain_url
   #explainResponse = Net::HTTP.get_response explainURI
   #puts explainResponse.body
